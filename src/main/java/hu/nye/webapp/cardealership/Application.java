c743095cd9a5a76801cde848f1f8be9eb36deed3
Istvan Gombos <gmbs.istvan@gmail.com>
2022-06-02 05:06:16 +0200

    alap projekt + application.yml

---
+++ src/main/java/hu/nye/webapp/cardealership/Application.java
@@ -0,0 +1,14 @@
+package hu.nye.webapp.cardealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("helló :)");
+    }
+
+}
